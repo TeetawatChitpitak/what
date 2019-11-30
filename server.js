ae2d05010fe33f24d7f78ba585636eb34615e8f6
Teetawat Chitpitak <sandna03@gmail.com>
2019-12-01 04:30:03 +0700

    add views and npm scrip fix versiont

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.port||3000
 
+app.set('view engine','ejs')
+
+app.get('/',(req,res,next)=>{
+    res.render('index')
+})
+
 app.listen(port,()=>{
     console.log(`working on port ${port}`);
 })
