8140abef25338beba43522b81aa889c7e4df1abf
Teetawat Chitpitak <sandna03@gmail.com>
2019-12-01 05:05:58 +0700

    query data from couch

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.port||3000
+const NodeCouchDb  = require('node-couchdb')
+const couch = new NodeCouchDb()
+const route = require('./route')(app,couch)
 
 app.set('view engine','ejs')
 
-app.get('/',(req,res,next)=>{
-    res.render('index')
-})
+
 
 app.listen(port,()=>{
     console.log(`working on port ${port}`);
