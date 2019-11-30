dcc4f352ca9a4a7fe55c1651fe3ea65de3671097
Teetawat Chitpitak <sandna03@gmail.com>
2019-12-01 05:07:08 +0700

    add route file

---
+++ route.js
@@ -0,0 +1,18 @@
+const dbname = 'store'
+module.exports = (app,couch)=>{
+    app.route('/')
+    .get((req,res,next)=>{
+        console.log('on this routing file'); 
+        res.render('index')
+    })
+    app.get('/1',async (req,res,next)=>{
+        try {
+            let viewurl = "_design/Items/_view/Items"
+            let data = await couch.get(dbname,viewurl)
+            let rows  = await data.data.rows
+            res.send(rows)
+        } catch (error) {
+         throw error   
+        } 
+    })
+}
