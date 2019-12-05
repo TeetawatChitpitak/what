744730b0c4d5a0dc56fc68ed63a90ab8eb726028
Teetawat Chitpitak <sandna03@gmail.com>
2019-12-06 02:06:26 +0700

    log error

--- route.js
+++ route.js
@@ -9,6 +9,8 @@ module.exports = (app,couch)=>{
             let viewurl = "_design/Items/_view/Items"
             let data = await couch.get(dbname,viewurl)
             let rows  = await data.data.rows
+            console.log(rows);
+            
             res.send(rows)
         } catch (error) {
          throw error   
