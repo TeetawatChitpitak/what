f2d26fd90ea10f6332c984753a0658e097d61aac
Teetawat Chitpitak <sandna03@gmail.com>
2019-12-19 04:32:38 +0700

    rm type script

--- route.js
+++ route.js
@@ -1,4 +1,5 @@
 const dbname = 'store'
+require('./test.ts')
 module.exports = (app,couch,couchAuth)=>{
     app.route('/')
     .get((req,res,next)=>{
@@ -7,12 +8,14 @@ module.exports = (app,couch,couchAuth)=>{
     app.get('/1',async (req,res,next)=>{
         try {
             console.log('......');
-            let viewurl = "_design/Items/_view/Items"
-            let data = await couch.get(dbname,viewurl)
-            let rows  = await data.data.rows
-            res.send(rows)
+            //let viewurl = "_design/Items/_view/Items"
+            // let data = await couch.get(dbname,viewurl)
+            // let rows  = await data.data.rows
+            // res.send(rows)
+            let t = new Test('qweqe')
+            t.shoute
         } catch (error) {
-         throw error   
+         //throw error   
         } 
     })
 }
