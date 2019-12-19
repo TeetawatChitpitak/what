357ba7d5ca8fce5571b2a0e491a3ab6f5fa61cd2
Teetawat Chitpitak <sandna03@gmail.com>
2019-12-19 10:19:58 +0700

    restore

--- route.js
+++ route.js
@@ -8,10 +8,10 @@ module.exports = (app,couch,couchAuth)=>{
     app.get('/1',async (req,res,next)=>{
         try {
             console.log('......');
-            //let viewurl = "_design/Items/_view/Items"
-            // let data = await couch.get(dbname,viewurl)
-            // let rows  = await data.data.rows
-            // res.send(rows)
+            let viewurl = "_design/Items/_view/Items"
+            let data = await couch.get(dbname,viewurl)
+            let rows  = await data.data.rows
+            res.send(rows)
             let t = new Test('qweqe')
             t.shoute
         } catch (error) {
